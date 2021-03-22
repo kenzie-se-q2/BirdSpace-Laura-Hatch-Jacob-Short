5a6592853d331fd5e55485f9d97251914adf8b49
Jacob Short <thatm@JacobsComputer>
2021-03-22 10:49:01 -0700

    fixed routing for feed

--- src/components/EyeBrow.js
+++ src/components/EyeBrow.js
@@ -1,59 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const EyeBrow = (props) => {
-	return (
-		<div class="container-fluid">
-		<div className="eye-brow">
-			<div class="container">
-				<nav aria-label="breadcrumb">
-					<ol class="breadcrumb">
-						<li class="breadcrumb-item active" aria-current="page">
-							<Link to="/">Home</Link>
-						</li>
-					</ol>
-				</nav>
-
-				<nav aria-label="breadcrumb">
-					<ol class="breadcrumb">
-						<li class="breadcrumb-item">
-							<a href="#">
-								<Link to="/">Home</Link>
-							</a>
-						</li>
-						<li class="breadcrumb-item active" aria-current="page">
-							<Link to="/registration">Sign Up</Link>
-						</li>
-					</ol>
-				</nav>
-
-				<nav aria-label="breadcrumb">
-					<ol class="breadcrumb">
-						<li class="breadcrumb-item">
-							<a href="#">
-								<Link to="/">Home</Link>
-							</a>
-						</li>
-						<li class="breadcrumb-item">
-							<a href="#">
-								<Link to="/registration">Sign Up</Link>
-							</a>
-						</li>
-						<li class="breadcrumb-item active" aria-current="page">
-							<Link to="/profile">Profile</Link>
-						</li>
-						<li class="breadcrumb-item active" aria-current="page">
-							<Link to="">Sign Out</Link>
-						</li>
-					</ol>
-				</nav>
-			</div>
-		</div>
-		</div>
-	);
+  return (
+    <div class="container-fluid">
+      <div className="eye-brow">
+        <div class="container">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <a href="#">
+                  <Link to="/">Home</Link>
+                </a>
+              </li>
+              <li class="breadcrumb-item">
+                <a href="#">
+                  <Link to="/registration">Sign Up</Link>
+                </a>
+              </li>
+			  <li class="breadcrumb-item active" aria-current="page">
+                <Link to="/settings">Settings</Link>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+                <Link to="/feed">Feed</Link>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+                <Link to="">Sign Out</Link>
+              </li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 };
-//
-// cannot export this component as default
 
 // Plan for today :
 // 1. Get my buttons working
@@ -81,18 +64,41 @@ export const EyeBrow = (props) => {
 // export default Navigation;
 
 {
-	/* <nav className='nav'>
+  /* <nav className='nav'>
             {/* {<Link to='/'>Home</Link>} */
 }
 {
-	/* <Link to='/registration'>Sign Up</Link>
+  /* <Link to='/registration'>Sign Up</Link>
             <Link to='/settings'>Settings</Link>
             <Link to=''>Sign Out</Link>
             <Link to='/profile'>Profile</Link> */
 }
 {
-	/* can do a ternary to conditionally render if logged in */
+  /* can do a ternary to conditionally render if logged in */
 }
 {
-	/* </nav> */
+  /* </nav> */
+}
+
+{
+  /* <nav aria-label="breadcrumb">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-item active" aria-current="page">
+							<Link to="/">Home</Link>
+						</li>
+					</ol>
+				</nav>
+
+				<nav aria-label="breadcrumb">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-item">
+							<a href="#">
+								<Link to="/">Home</Link>
+							</a>
+						</li>
+						<li class="breadcrumb-item active" aria-current="page">
+							<Link to="/registration">Sign Up</Link>
+						</li>
+					</ol>
+				</nav> */
 }
