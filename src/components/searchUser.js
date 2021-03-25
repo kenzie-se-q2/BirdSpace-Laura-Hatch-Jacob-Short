7018a290ab9c22718040f202cb6d73b07f79196c
Ladeeluc <laura.hatch.lh@gmail.com>
2021-03-25 18:44:08 -0400

    Added in updates for the searchUser.js

--- src/components/searchUser.js
+++ src/components/searchUser.js
@@ -5,15 +5,19 @@ import React from 'react';
 
 export const searchUser = (props) => {
     return (
+
   <div className='container-xxl'> 
-  <div className= ' '    
+  <div className= 'laura-searchuser'>   
 <div className="input-group mb-3">
   <button className="btn btn-outline-secondary" 
   type="button" id="button-addon1">Search for User</button>
   <input type="text" className="form-control" placeholder="" 
   aria-label="Example text with button addon" aria-describedby="button-addon1"/>
 </div>
-</div> 
+</div>
+</div>
+
+
 
     )
 } 
